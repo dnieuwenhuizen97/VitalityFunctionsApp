85c3828ed3b6fecc61da7e946a6ee41c4089cff3
Bryan Gunther <bryan.gunther@hotmail.com>
2022-02-16 15:53:23 +0100

    Started restructuring the activity/challenge part

---
+++ Domains/Activity.cs
@@ -0,0 +1,82 @@
+﻿using Domains.DTO;
+using Domains.Enums;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Resolvers;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domains
+{
+    public class Activity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        [OpenApiProperty(Description = "Gets or sets the activity ID")]
+        public string ActivityId { get; set; }
+
+        [MaxLength(450)]
+        [OpenApiProperty(Description = "Gets or sets the comment's timelinepost id")]
+        public virtual ActivityCategory Category { get; set; }
+
+
+        [MaxLength(150)]
+        [OpenApiProperty(Description = "Gets or sets the activity title")]
+        public string Title { get; set; }
+
+        [OpenApiProperty(Description = "Gets or sets the activity type")]
+        public ChallengeType ChallengeType { get; set; }
+
+        [MaxLength(500)]
+        [OpenApiProperty(Description = "Gets or sets the activity description")]
+        public string Description { get; set; }
+
+        [MaxLength(500)]
+        [OpenApiProperty(Description = "Gets or sets the activity image")]
+        public string ImageLink { get; set; }
+
+        [MaxLength(500)]
+        [OpenApiProperty(Description = "Gets or sets the activity video")]
+        public string VideoLink { get; set; }
+
+        [OpenApiProperty(Description = "Gets or sets the activity startDate")]
+        public DateTime StartDate { get; set; }
+
+        [OpenApiProperty(Description = "Gets or sets the activity endDate")]
+        public DateTime EndDate { get; set; }
+
+        [OpenApiProperty(Description = "Gets or sets the activity url")]
+        public string Url { get; set; }
+
+        [MaxLength(100)]
+        [OpenApiProperty(Description = "Gets or sets the activity location")]
+        public string Location { get; set; }
+
+        [OpenApiProperty(Description = "Gets or sets the activity points")]
+        public int Points { get; set; }
+
+        public virtual ICollection<SubscribedChallenge> SubscribedChallenges { get; set;}
+
+        public Activity()
+        {
+
+        }
+
+        public Activity(string title, ChallengeType challengeType, string description, DateTime startDate, DateTime endDate, string location, int points)
+        {
+            this.ActivityId = Guid.NewGuid().ToString();
+            this.Title = title;
+            this.ChallengeType = challengeType;
+            this.Description = description;
+            this.StartDate = startDate;
+            this.EndDate = endDate;
+            this.Location = location;
+            this.Points = points;
+        }
+    }
+}
+
---
+++ Infrastructure/Migrations/20220216105350_ActivityCategories.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class ActivityCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ActivityId",
+                table: "SubscribedChallenge",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ActivityCategory",
+                columns: table => new
+                {
+                    CategoryId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    ImageLink = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActivityCategory", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Activity",
+                columns: table => new
+                {
+                    ActivityId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CategoryId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    ChallengeType = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    ImageLink = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    VideoLink = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Points = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activity", x => x.ActivityId);
+                    table.ForeignKey(
+                        name: "FK_Activity_ActivityCategory_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "ActivityCategory",
+                        principalColumn: "CategoryId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubscribedChallenge_ActivityId",
+                table: "SubscribedChallenge",
+                column: "ActivityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activity_CategoryId",
+                table: "Activity",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubscribedChallenge_Activity_ActivityId",
+                table: "SubscribedChallenge",
+                column: "ActivityId",
+                principalTable: "Activity",
+                principalColumn: "ActivityId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubscribedChallenge_Activity_ActivityId",
+                table: "SubscribedChallenge");
+
+            migrationBuilder.DropTable(
+                name: "Activity");
+
+            migrationBuilder.DropTable(
+                name: "ActivityCategory");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SubscribedChallenge_ActivityId",
+                table: "SubscribedChallenge");
+
+            migrationBuilder.DropColumn(
+                name: "ActivityId",
+                table: "SubscribedChallenge");
+        }
+    }
+}
